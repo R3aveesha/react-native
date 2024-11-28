fa62517060c3a50df04e7ed0d1c04b05efc4cde7
raveesha gimhan <146311393+R3aveesha@users.noreply.github.com>
2024-11-28 22:44:39 +0530

    updated

---
+++ myFirst_app/app/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+
+const index = () => {
+  return (
+    <View>
+      <Text style={{
+        color:"red",
+        fontSize:27,
+      }}>Index</Text>
+    </View>
+  )
+}
+
+export default index
