ee8add0f3fd5d57ed7856cd7a7dd25b6b67a8f2f
raveesha gimhan <146311393+R3aveesha@users.noreply.github.com>
2024-11-29 12:58:27 +0530

    update

--- myFirst_app/app/index.tsx
+++ myFirst_app/app/index.tsx
@@ -14,18 +14,20 @@ const index = () => {
         fontSize:27,
       }}>Index</Text>
 
-      <button style={{padding:'10px',backgroundColor:"#2e4c64",border:"none",borderRadius:"100px"}}><Link href={"/Category"}>Category</Link></button>
-      <button style={{padding:'10px',backgroundColor:"#4396d8",border:"none",borderRadius:"100px"}}><Link href={"/Product"}>Product</Link></button>
-      <button style={{padding:'10px',backgroundColor:"#4396d8",border:"none",borderRadius:"100px"}}><Link href={"/123"}>Product123</Link></button>
+      <Link href={"/Category"}>Category</Link>
+      <Link href={"/Product"}>Product</Link>
+      <Link href={"/123"}>Product123</Link>
 
       <Link href={{
         pathname:"/[productId]",
         params:{
           productId:"auk",
-          name:"Amila",
-          city:"wadduwa"
+          name:"raveesha",
+          city:"kalutara"
         }
       }}>click here</Link>
+
+      
 
 
 
